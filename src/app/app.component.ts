aefc4eb8382b334c0f50307ea1fd70376dddd526
Hyena2001 <z399wang@uwaterloo.ca>
2025-04-21 00:29:13 -0400

    version 1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  standalone: true,
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'parkchamp-anagram';
+export class AppComponent implements OnInit {
+  entries: string[] = [];
+  resultCount = 0;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    const url = 'https://parkchampanagramchecker-default-rtdb.firebaseio.com/test.json';
+    this.http.get<{ [key: string]: string | null }>(url)
+      .subscribe(data => {
+          this.entries = Object.values(data)
+            .filter((v): v is string => v != null && typeof v === 'string');
+
+          console.log('filter entries:', this.entries);
+        },
+        err => console.error('ERROR:', err));
+  }
+
+  onQuery(query: string) {
+    const sorted = (s: string) => s.split('').sort().join('');
+    this.resultCount = this.entries
+      .filter(w => sorted(w) === sorted(query) && w !== query)
+      .length;
+  }
 }
